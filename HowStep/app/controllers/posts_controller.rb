09729b1ab34168b94eacff3fc2fd6b3d76dbb952
woarewe <rostislav.zhuravsky@gmail.com>
2017-08-01 01:53:43 +0300

    Ass steps and posts(model)

---
+++ HowStep/app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
