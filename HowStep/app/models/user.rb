8581c98da780e02265cb1ff726020b3f00fa8d8d
woarewe <rostislav.zhuravsky@gmail.com>
2017-08-01 01:15:01 +0300

    regisration with media

---
+++ HowStep/app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook, :twitter, :vkontakte, :google_oauth2]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      set_user_data(user, auth)
+    end
+  end
+
+  private
+
+  def self.set_user_data(user, auth)
+    user.name = auth.info.name
+    user.image = auth.info.image
+    user.password = Devise.friendly_token[0, 20]
+  end
+end
