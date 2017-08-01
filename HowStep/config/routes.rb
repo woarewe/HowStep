f392c141c97ebd12dcf719ead29dc3b203b9153c
woarewe <zhuravskyr@gmail.com>
2017-08-01 16:39:00 +0300

    add steps controller

--- HowStep/config/routes.rb
+++ HowStep/config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :users, except: [:new]
-  resources :posts
+  resources :posts do
+    resources :steps, except: [:index]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
