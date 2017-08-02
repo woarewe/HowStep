7de9a85f1fcb02c701bb795c59edea8702f1064f
woarewe <rostislav.zhuravsky@gmail.com>
2017-08-02 05:30:20 +0300

    Add user panel

--- HowStep/app/controllers/posts_controller.rb
+++ HowStep/app/controllers/posts_controller.rb
@@ -27,9 +27,23 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def update
+    @post = Post.find(params[:id])
+    check_update(@post)
+    render 'edit'
+  end
+
   private
+
   def post_params
     params.require(:post).permit(:title)
   end
 
+  def check_update(post)
+    if post.update_attributes(post_params)
+      flash.now[:notice] = t('controllers.posts.update.notice')
+    else
+      flash.now[:alert] = t('controllers.posts.update.alert')
+    end
+  end
 end
