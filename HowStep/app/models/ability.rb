9df2356793f3935b8f86948d33de799ba3bc4b91
woarewe <rostislav.zhuravsky@gmail.com>
2017-08-06 17:44:11 +0300

    add categories link

--- HowStep/app/models/ability.rb
+++ HowStep/app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
+    user ||= User.new # guest user (not logged in)
     can :read, :all
     if user.admin?
       can :manage, :all
