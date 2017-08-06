845a1258da1dfe080106e5061e5fb26f798cfd9d
woarewe <rostislav.zhuravsky@gmail.com>
2017-08-06 21:56:54 +0300

    Add search

--- HowStep/app/models/post.rb
+++ HowStep/app/models/post.rb
@@ -1,8 +1,17 @@
 class Post < ApplicationRecord
+  include SearchCop
+
+  validates :title, presence: true
   belongs_to :user
   belongs_to :category
   has_many :steps, -> { order(position: :asc) }, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  search_scope :search do
+    attributes :title
+    attributes comment: 'comments.content'
+    attributes step: 'steps.content'
+  end
 
   acts_as_taggable
   ratyrate_rateable 'content'
