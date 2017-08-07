f033248958ddfedea499cccfb8c73a77b26a0b26
woarewe <rostislav.zhuravsky@gmail.com>
2017-08-07 06:06:20 +0300

    fix fronend

--- HowStep/app/controllers/categories_controller.rb
+++ HowStep/app/controllers/categories_controller.rb
@@ -1,11 +1,13 @@
 class CategoriesController < ApplicationController
   def create
     Category.create(params.require(:category).permit(:title))
-    redirect_to root_path
+    redirect_to categories_path
   end
 
   def destroy
     Category.find(params[:id]).destroy
+    flash[:notice] = t('controllers.categories.destroy.notice')
+    redirect_to categories_path
   end
 
   def new
